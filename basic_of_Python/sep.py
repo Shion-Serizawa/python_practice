bd7d3d6cb2692320d2ac0f4d6836c0d9ce095489
Shion-Serizawa <61482303+Shion-Serizawa@users.noreply.github.com>
2021-07-22 16:07:36 +0900

    printの終了の設定を追加

--- basic_of_Python/sep.py
+++ basic_of_Python/sep.py
@@ -3,5 +3,7 @@ num2 = '2'
 
 total = int(num) + int(num2)
 
-print(total, type(total),sep = ",")
+print(total, type(total), sep = ",", end = "")
+print("「つながっちゃった！」", end="..\n")
+print("こんどはピリオド二つと改行")
 
